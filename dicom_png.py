5c9520f7cb9ea0e8874694b0553363541d143dc7
Anne-Andresen <56411446+Anne-Andresen@users.noreply.github.com>
2021-11-21 14:59:56 +0100

    Update dicom_png.py

--- dicom_png.py
+++ dicom_png.py
@@ -1,23 +1,20 @@
 import os
-# dcm_folder= 'D:\Patient-1\Patient_1\Dec 9, 2020/registered/'
-# PNG_folder= '/Users/riaroque/Desktop/PNG folder'
 import pydicom as dicom
 import os
 import cv2
 import numpy as np
-import PIL # optional
-from pydicom.pixel_data_handlers.util import apply_voi_lut
-from skimage import exposure
+
+
 
 
 
 
 # make it True if you want in PNG format
 PNG = True
-# Specify the .dcm folder path
-folder_path = "D:\Patient-1\Patient_1\Dec 9, 2020/registered/"
-# Specify the output jpg/png folder path
-jpg_folder_path = "D:\Patient-1\Patient_1\Dec 9, 2020/png/"
+# Specify the folder path to DICOM imgs here
+folder_path = " DCM path here"
+# The output path of png /jpeg 
+jpg_folder_path = "PNG/jpeg path here"
 images_path = os.listdir(folder_path)
 for n, image in enumerate(images_path):
     ds = dicom.dcmread(os.path.join(folder_path, image))
