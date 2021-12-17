d93190d3e512c32181a33e8cab8dffbb77f15a13
Anne-Andresen <56411446+Anne-Andresen@users.noreply.github.com>
2021-12-17 13:50:13 +0100

    Update template_matching.py

--- template_matching.py
+++ template_matching.py
@@ -15,7 +15,7 @@ def get_numbers_from_filename(input):
 
 # Missing need to exract specific part of file name
 
-for f in glob.glob('E:/AAU/tracking - Copy - Copy/Dec 20, 2019/scans/patches/predict/p*.png'):
+for f in glob.glob('path/*.png'):
     print('Loading new patch')
     img = cv.imread(f)
     gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
@@ -29,25 +29,7 @@ for f in glob.glob('E:/AAU/tracking - Copy - Copy/Dec 20, 2019/scans/patches/pre
     # print('File:', file)
     y = os.path.splitext(file)[0]
    #  print('Y_ ', y)
-    '''
-    z = file.split('2019')
-    print('Z[0]', z[0], 'Z[1]')
-    na = z[0]
-    r = file.split('IMG00')[-1]
-    s = str(y)
-    s1 = r[0]
-    s2 = r[1]
-    print('S1',s1,'S2', s2)
-    name = s.rpartition(s1 + s2)
-    patch_number = name[2]
-    slice_number = name[1]
-    name1 = name[0]
-    print('name', name[0])
-    print('Name ', patch_number)
-    print('Slice', slice_number)
-    #pit = patch_number.split('_')
-    #print('pit', pit[0])
-    '''
+
     E = [int(x) for x in re.findall('\d+', file)]
     print('E', E)
     print('ZZ', E[4], 'RR', E[5])
@@ -73,13 +55,13 @@ for f in glob.glob('E:/AAU/tracking - Copy - Copy/Dec 20, 2019/scans/patches/pre
         print('patch is black')
     else:
         #hi= z[0]
-        path1 = 'E:/AAU/tracking - Copy - Copy/Dec 20, 2019/scans/patches/'+file
+        path1 = 'template path'+file
         patch = cv.imread(path1)
         print('patch', patch)
-        path12 = 'E:/AAU/tracking - Copy - Copy/Jan 3, 2020/scans/gt_IMG00'
+        path12 = 'image path'
         print('path12', path12)
 
-        path = 'E:/AAU/tracking - Copy - Copy/Jan 3, 2020/scans/gt_IMG00'
+        path = ''
         dir1 = os.path.dirname(os.path.dirname(path))
         print('Directory path1 ', dir1)
         dirname12 = os.path.split(dir1)[1]
@@ -144,47 +126,4 @@ for f in glob.glob('E:/AAU/tracking - Copy - Copy/Dec 20, 2019/scans/patches/pre
 
         # cv.moveWindow('Template', 10, 50);
         # cv.moveWindow('Result', 150, 50);
-        '''
 
-        print('Here')
-        cv.waitKey(0)
-        cv.destroyAllWindows()
-        cv.waitKey(1)
-        # img = os.path.join(path1, '00060.pnh')
-        '''
-
-
-'''
-text.rpartition('offset ')
-Out[179]: ('March 1st 2013 ntp[22485] Time server ', 'offset ', '-.0070 sec')
-In [169]: text.rpartition('offset ')[-1]
-Out[169]: '-.0070 sec'
-'''
-
-'''
-template = cv.imread('mergmerg_448_384.png')
-img = cv.imread('merged1.png')
-# Works for one lymph node with boundaries  not entirely sure how it will work for multiple.
-img_gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-template_gray = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
-
-result = cv.matchTemplate(img_gray, template_gray, cv.TM_CCOEFF)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-top_left = max_loc
-h, w = template_gray.shape
-
-bottom_right = (top_left[0] + w, top_left[1] + h)
-cv.rectangle(img, top_left, bottom_right, (0, 0, 255), 4)
-cv.putText(img, 'LN-1', top_left,cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-cv.imwrite('BB.png', img)
-cv.imshow('Result', img)
-cv.imshow('Template', template)
-# cv.moveWindow('Template', 10, 50);
-# cv.moveWindow('Result', 150, 50);
-
-print('Here')
-cv.waitKey(0)
-cv.destroyAllWindows()
-cv.waitKey(1)
-# img = os.path.join(path1, '00060.pnh')
-'''
